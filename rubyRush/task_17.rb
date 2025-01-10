c5159231dcea21cf52274614e00653b4dfee45ff
MikholapIvan <ivanmikholap6@gmail.com>
2025-01-10 18:08:22 +0300

    commit task_17

---
+++ rubyRush/task_17.rb
@@ -0,0 +1,10 @@
+puts "Введите число 1:\n> "
+a = gets.to_i
+puts "Введите число 2:\n> "
+b = gets.to_i
+puts "Введите число 3:\n> "
+c = gets.to_i
+
+avg = (a + b + c) / 3
+
+puts "\n\nСреднее арифметическое: #{avg}"
