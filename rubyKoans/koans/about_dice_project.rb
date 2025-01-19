5fe7ab3a7c1a0dd33d0322847bf880f3c2bee8e3
MikholapIvan <ivanmikholap6@gmail.com>
2025-01-19 16:44:52 +0300

    solve about_dice_project and update path_progress

--- rubyKoans/koans/about_dice_project.rb
+++ rubyKoans/koans/about_dice_project.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+  
+  def roll(number)
+    @values = []
+    for i in (0..(number - 1))
+      @values << rand(6) + 1
+    end
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
